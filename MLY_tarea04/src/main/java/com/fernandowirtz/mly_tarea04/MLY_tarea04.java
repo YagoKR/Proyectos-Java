5b77eb8f366ca8245eb8e1c9818bdbdc780b3d73
YagoKR <164676993+YagoKR@users.noreply.github.com>
2025-01-08 21:09:58 +0100

    Add files via upload

---
+++ MLY_tarea04/src/main/java/com/fernandowirtz/mly_tarea04/MLY_tarea04.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.fernandowirtz.mly_tarea04;
+
+/**
+ *
+ * @author yago.martinezloureda
+ */
+public class MLY_tarea04 {
+
+    public static void main(String[] args) {
+        Supermercado supermercado = new Supermercado(3);
+        supermercado.generarClientes();
+    }
+}
